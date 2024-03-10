edbcb9a0cdfa57f519d33649ec8ff5560d91bf16
Kunal Ahire <123294963+kunalahire1@users.noreply.github.com>
2024-03-10 18:37:10 +0530

    Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,31 @@
 package com.driver;
-
+import java.util.*;
 public class Main {
-
+    
+    public static void main(String[] args) {
+      
+      Product p=new Product();
+         System.out.println( p.product(1,2));
+         System.out.println(p.product(1,2,3));
+         System.out.println(p.product(23.22,34.22));
+     
+    }
+    public static class Product{
+        public int product(int x, int y) {
+            return x+y;
+        }
+        
+    
+   //  @overloaded
+      public int product(int x, int y, int z) {
+          return x+y+z;
+          
+      }
+    //  @overloaded
+      public double product(double x, double y) {
+          return x+y;
+          
+      }
+      }
 }
+
