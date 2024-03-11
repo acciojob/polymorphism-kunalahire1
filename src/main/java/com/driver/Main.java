e86646baca0ce60a290118a92dc5fc53f47c0861
Kunal Ahire <123294963+kunalahire1@users.noreply.github.com>
2024-03-11 10:27:16 +0530

    Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Main {
     
     public static void main(String[] args) {
+        
       
       Product p=new Product();
          System.out.println( p.product(1,2));
